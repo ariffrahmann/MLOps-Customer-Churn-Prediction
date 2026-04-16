dea658fc14ef635fdd11c40a3ca5aa7891742e86
Arif Rahman <arifrahman23204@student.ub.ac.id>
2026-04-16 16:44:50 +0000

    init-dvc

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -1,25 +1,39 @@
 import pandas as pd
-from datetime import datetime
 import os
 
 RAW_DIR = "data/raw"
+OUTPUT_FILE = "dataset.csv"
+# jumlah data per ingest
+CHUNK_SIZE = 100  
 
 def ingest_data(source_path):
-    # baca data
+    # baca data baru
     if source_path.endswith(".csv"):
-        df = pd.read_csv(source_path)
+        new_df = pd.read_csv(source_path, nrows=CHUNK_SIZE)
     elif source_path.endswith(".xlsx"):
-        df = pd.read_excel(source_path)
+        new_df = pd.read_excel(source_path, nrows=CHUNK_SIZE)
     else:
         raise ValueError("Format file tidak didukung")
 
-    # timestamp
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    output_path = os.path.join(RAW_DIR, f"data_{timestamp}.csv")
+    output_path = os.path.join(RAW_DIR, OUTPUT_FILE)
 
-    df.to_csv(output_path, index=False)
+    # kalau dataset sudah ada → append
+    if os.path.exists(output_path):
+        old_df = pd.read_csv(output_path)
+        combined_df = pd.concat([old_df, new_df], ignore_index=True)
+
+        print(f"Dataset lama: {len(old_df)} baris")
+        print(f"Data baru (batch): {len(new_df)} baris")
+        print(f"Total setelah update: {len(combined_df)} baris")
+    else:
+        combined_df = new_df
+        print(f"Dataset baru dibuat: {len(new_df)} baris")
+
+    # simpan
+    combined_df.to_csv(output_path, index=False)
 
     print(f"Data berhasil disimpan ke: {output_path}")
+
 
 if __name__ == "__main__":
     ingest_data("data/external/ecommerce_churn_kaggle.csv")
